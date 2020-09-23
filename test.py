22974b37bace7a8a1a3ce6decc94822bb328e77b
Michel Corne <mcorne@yahoo.com>
2020-09-23 18:02:48 +0200

    added pandas tutorial

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
-tel = {'emmanuelle': 5752, 'sebastian': 5578}
-tel['francis'] = 5915
-tel.keys()
+import pandas as pd
+
+df = pd.DataFrame(
+    {
+        "Name": [
+            "Braund, Mr. Owen Harris",
+            "Allen, Mr. William Henry",
+            "Bonnell, Miss. Elizabeth",
+        ],
+        "Age": [22, 35, 58],
+        "Points": [10, 11, 15],
+        "Sex": ["male", "male", "female"],
+    }
+)
+a = df.describe()
+print(a)

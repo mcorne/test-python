89a1064d6dd54959d43e60750d37cdac7d91bd3e
Michel Corne <mcorne@yahoo.com>
2020-11-09 15:17:07 +0100

    fixed leetcode problem

--- leetcode/0866. Prime Palindrome.py
+++ leetcode/0866. Prime Palindrome.py
@@ -1,27 +1,42 @@
+from bisect import bisect_left
 from math import sqrt
+
 class Solution:
+    palindromes = None
+
+    def __init__(self) -> None:
+        if not Solution.palindromes:
+            Solution.palindromes = self.generatePalindromes()
+
+    # https://leetcode.com/problems/prime-palindrome/discuss/868157/Python-creating-palindrome-list
+    def generatePalindromes(self) -> list:
+        pals = [[] for i in range(9)]
+        pals[0] = ['0','1','2','3','4','5','6','7','8','9']
+        pals[1] =['00','11','22','33','44','55','66','77','88','99']
+        for i in range(2,8):
+            pals[i] = [x[0]+y+x[1] for y in pals[i-2] for x in pals[1]]
+        pals[8] = ['1'+y+'1' for y in pals[6]] # need only 10^8 range
+        palindromes = sorted(int(x) for y in pals for x in y)
+        return palindromes
+
+    # see https://en.wikipedia.org/wiki/Trial_division
+    def isPrime(self, N: int) -> bool:
+        if N % 2 == 0:
+            return False
+        i = 3
+        sqrt_N = sqrt(N)
+        while i <= sqrt_N:
+            if N % i == 0:
+                return False
+            i += 2
+        return True
+
     def primePalindrome(self, N: int) -> int:
         if N == 1 or N == 2:
             return 2
-        if N == 3:
-            return 3
-        if N % 2 == 0:
-            N += 1
-        while True:
-            s = str(N)
-            if s == s[::-1]:
-                i = 3
-                is_prime = True
-                # see https://en.wikipedia.org/wiki/Trial_division
-                sqrt_N = sqrt(N)
-                while i <= sqrt_N:
-                    if N % i == 0:
-                        is_prime = False
-                        break
-                    i += 2
-                if is_prime:
-                    return N
-            N += 2
+        for N in Solution.palindromes[bisect_left(Solution.palindromes,N):]:
+            if self.isPrime(N):
+                return(N)
 
 # Tests
 solution = Solution()
@@ -35,15 +50,17 @@ N = 1
 print(solution.primePalindrome(N))
 N = 2
 print(solution.primePalindrome(N))
+N = 3
+print(solution.primePalindrome(N))
 N = 1000000
 print(solution.primePalindrome(N))
 N = 9989900
 print(solution.primePalindrome(N))
 
 # LeetCode Submission
-# Error: Time Limit Exceeded.
-# 51 / 60 test cases passed.
-# Last executed input: 9989900
+# Runtime: 56 ms, faster than 87.95% of Python3 online submissions for Prime Palindrome.
+# Memory Usage: 17 MB, less than 9.64% of Python3 online submissions for Prime Palindrome.
+# 60 / 60 test cases passed.
 
 # https://primes.utm.edu/howmany.html
 # 5,761,455 prime numbers <= 10^8

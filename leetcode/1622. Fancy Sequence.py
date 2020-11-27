56128c0229d0b88425097b4ca25532aab07cc069
Michel Corne <mcorne@yahoo.com>
2020-11-27 16:17:02 +0100

    fixed leetcode problem

--- leetcode/1622. Fancy Sequence.py
+++ leetcode/1622. Fancy Sequence.py
@@ -1,38 +1,32 @@
 class Fancy:
     def __init__(self):
-        self.actions = []
-        self.index = 0
+        self.operations = []
         self.values = []
 
-    def append(self, val: int) -> None:
-        self.values.append([self.index, val])
-        self.index += 1
-
     def addAll(self, inc: int) -> None:
-        self.actions.append([self.index, inc, "+"])
-        self.index += 1
+        self.operations.append([inc, "+"])
 
-    def multAll(self, m: int) -> None:
-        self.actions.append([self.index, m, "*"])
-        self.index += 1
+    def append(self, value: int) -> None:
+        self.values.append([value, len(self.operations)])
 
     def getIndex(self, idx: int) -> int:
         if idx >= len(self.values):
             return -1
-        val = self.updateValue(idx)
-        return val % (10**9 + 7)
+        if self.values[idx][1] < len(self.operations):
+            self.updateValue(idx)
+        return self.values[idx][0] % (10**9 + 7)
 
-    def updateValue(self, idx):
-        val_idx, val = self.values[idx]
-        if val_idx < self.index:
-            for action_idx, val_op, op in self.actions:
-                if action_idx > val_idx:
-                    if op == "+":
-                        val += val_op
-                    else: # *
-                        val *= val_op
-            self.values[idx] = [self.index-1, val]
-        return val
+    def multAll(self, m: int) -> None:
+        self.operations.append([m, "*"])
+
+    def updateValue(self, value_idx: int)-> int:
+        value, operation_idx = self.values[value_idx]
+        for operand, operator in self.operations[operation_idx:]:
+            if operator == "+":
+                value += operand
+            else:
+                value *= operand
+        self.values[value_idx] = [value, len(self.operations)]
 
     @classmethod
     def generateSequence(cls, methods: list, args: list) -> list:
@@ -63,9 +57,29 @@ args = [[], [2], [3], [7], [2], [0], [3], [10], [2], [0], [1], [2]]
 expected = [None, None, None, None, None, 10, None, None, None, 26, 34, 20]
 test(methods, args, expected)
 
+methods = ["Fancy", "addAll", "append", "multAll", "getIndex"]
+args = [[], [3], [7], [2], [0]]
+expected = [None, None, None, None, 14]
+test(methods, args, expected)
+
+methods = ["Fancy"]
+args = [[]]
+expected = [None]
+test(methods, args, expected)
+
+methods = ["Fancy", "append", "multAll", "multAll", "getIndex", "getIndex"]
+args = [[], [1], [2], [3], [0], [0]]
+expected = [None, None, None, None, 6, 6]
+test(methods, args, expected)
+
+methods = ["Fancy", "append", "multAll", "multAll", "getIndex", "append", "multAll", "multAll", "getIndex"]
+args = [[], [1], [2], [3], [0], [1], [2], [3], [1]]
+expected = [None, None, None, None, 6, None, None, None, 6]
+test(methods, args, expected)
+
 methods = ["Fancy"] + ["append", "addAll", "append", "multAll", "getIndex", "addAll", "append", "multAll", "getIndex", "getIndex"] * 4000
 args = [[]] + [[2], [3], [7], [2], [0], [3], [10], [2], [0], [1]] * 4000
 test(methods, args)
 
 # LeetCode Submission
-# Error: Time Limit Exceeded with 49003 methods/args (OK though when running test alone: 8512 ms)
+# Error: Time Limit Exceeded with 49002 methods/args at 104th test (OK though when running test alone: 6600 ms)

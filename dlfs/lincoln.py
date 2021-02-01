fdea48ab288434f9e56f3617a0a5cf4053bb1f76
Michel Corne <mcorne@yahoo.com>
2021-02-01 16:50:46 +0100

    added dlfs examples

--- dlfs/lincoln.py
+++ dlfs/lincoln.py
@@ -136,6 +136,75 @@ class Dropout(Operation):
     def _input_grad(self, output_grad: ndarray) -> ndarray:
         return output_grad * self.mask
 
+class Flatten(Operation):
+    def _output(self, inference: bool = False) -> ndarray:
+        return self.input_.reshape(self.input_.shape[0], -1)
+
+    def _input_grad(self, output_grad: ndarray) -> ndarray:
+        return output_grad.reshape(self.input_.shape)
+
+class Conv2D_Op(ParamOperation):
+    def __init__(self, W: ndarray):
+        super().__init__(W)
+        self.param_size = W.shape[2]
+        self.param_pad = self.param_size // 2
+
+    def _pad_1d(self, inp: ndarray) -> ndarray:
+        z = np.array([0])
+        z = np.repeat(z, self.param_pad)
+        return np.concatenate([z, inp, z])
+
+    def _pad_1d_batch(self, inp: ndarray) -> ndarray:
+        outs = [self._pad_1d(obs) for obs in inp]
+        return np.stack(outs)
+
+    def _pad_2d_obs(self, inp: ndarray):
+        inp_pad = self._pad_1d_batch(inp)
+        other = np.zeros((self.param_pad, inp.shape[0] + self.param_pad * 2))
+        return np.concatenate([other, inp_pad, other])
+
+    def _pad_2d_channel(self, inp: ndarray):
+        return np.stack([self._pad_2d_obs(channel) for channel in inp])
+
+    def _get_image_patches(self, input_: ndarray):
+        imgs_batch_pad = np.stack([self._pad_2d_channel(obs) for obs in input_])
+        patches = []
+        img_height = imgs_batch_pad.shape[2]
+        for h in range(img_height-self.param_size+1):
+            for w in range(img_height-self.param_size+1):
+                patch = imgs_batch_pad[:, :, h:h+self.param_size, w:w+self.param_size]
+                patches.append(patch)
+        return np.stack(patches)
+
+    def _output(self, inference: bool = False):
+        batch_size = self.input_.shape[0]
+        img_height = self.input_.shape[2]
+        img_size = self.input_.shape[2] * self.input_.shape[3]
+        patch_size = self.param.shape[0] * self.param.shape[2] * self.param.shape[3]
+        patches = self._get_image_patches(self.input_)
+        patches_reshaped = patches.transpose(1, 0, 2, 3, 4).reshape(batch_size, img_size, -1)
+        param_reshaped = self.param.transpose(0, 2, 3, 1).reshape(patch_size, -1)
+        output_reshaped = np.matmul(patches_reshaped, param_reshaped).reshape(batch_size, img_height, img_height, -1).transpose(0, 3, 1, 2)
+        return output_reshaped
+
+
+    def _input_grad(self, output_grad: np.ndarray) -> np.ndarray:
+        batch_size = self.input_.shape[0]
+        img_size = self.input_.shape[2] * self.input_.shape[3]
+        img_height = self.input_.shape[2]
+        output_patches = self._get_image_patches(output_grad).transpose(1, 0, 2, 3, 4).reshape(batch_size * img_size, -1)
+        param_reshaped = self.param.reshape(self.param.shape[0], -1).transpose(1, 0)
+        return np.matmul(output_patches, param_reshaped).reshape(batch_size, img_height, img_height, self.param.shape[0]).transpose(0, 3, 1, 2)
+
+
+    def _param_grad(self, output_grad: ndarray) -> ndarray:
+        batch_size = self.input_.shape[0]
+        img_size = self.input_.shape[2] * self.input_.shape[3]
+        in_channels = self.param.shape[0]
+        out_channels = self.param.shape[1]
+        in_patches_reshape = self._get_image_patches(self.input_).reshape(batch_size * img_size, -1).transpose(1, 0)
+        out_grad_reshape = output_grad.transpose(0, 2, 3, 1).reshape(batch_size * img_size, -1)
+        return np.matmul(in_patches_reshape, out_grad_reshape).reshape(in_channels, self.param_size, self.param_size, out_channels).transpose(0, 3, 1, 2)
 
 class Layer:
     def __init__(self, neurons: int):
@@ -173,6 +242,33 @@ class Layer:
         for operation in self.operations:
             if issubclass(operation.__class__,ParamOperation):
                 self.param_grads.append(operation.param_grad)
+
+class Conv2D(Layer):
+    def __init__(self, out_channels: int, param_size: int, dropout: int = 1.0, weight_init: str = "normal", activation: Operation = Linear(), flatten: bool = False) -> None:
+        super().__init__(out_channels)
+        self.param_size = param_size
+        self.activation = activation
+        self.flatten = flatten
+        self.dropout = dropout
+        self.weight_init = weight_init
+        self.out_channels = out_channels
+
+    def _setup_layer(self, input_: ndarray) -> ndarray:
+        self.params = []
+        in_channels = input_.shape[1]
+        if self.weight_init == "glorot":
+            scale = 2/(in_channels + self.out_channels)
+        else:
+            scale = 1.0
+        conv_param = np.random.normal(loc=0, scale=scale, size=(input_.shape[1], self.out_channels, self.param_size, self.param_size))
+        self.params.append(conv_param)
+        self.operations = []
+        self.operations.append(Conv2D_Op(conv_param))
+        self.operations.append(self.activation)
+        if self.flatten:
+            self.operations.append(Flatten())
+        if self.dropout < 1.0:
+            self.operations.append(Dropout(self.dropout))
 
 class Dense(Layer):
     def __init__(self, neurons: int, activation: Operation = Sigmoid(), conv_in=False, dropout=1.0, weight_init="standard"):

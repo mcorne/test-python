9da3f030a74a5b4938580f68bc57427b1d6e0c70
Michel Corne <mcorne@yahoo.com>
2021-03-11 10:38:10 +0100

    fixed dgfip examples

--- dgfip/somme-naturels-recursive.py
+++ dgfip/somme-naturels-recursive.py
@@ -7,5 +7,9 @@ def calc_sum(a, i = 0):
         sum = calc_sum(a, i+1)
     return sum
 
+a = [5]
+print(calc_sum(a))
+a = [-5]
+print(calc_sum(a))
 a = [1, -5, 3, 6, -8, 6]
 print(calc_sum(a))
--- dgfip/somme-sous-tableau.py
+++ dgfip/somme-sous-tableau.py
@@ -1,4 +1,4 @@
-def calc_sum(i, a, length):
+def calc_sub_sum(i, a, length):
     sum = 0
     j = i
     max_sum = None
@@ -11,18 +11,30 @@ def calc_sum(i, a, length):
         j += 1
     return [max_sum, max_j]
 
-a = [-8, -1, 5, -3, 8, -2, -10, 9]
-max_i = 0
-max_j = 0
-max_sum = a[0]
-length = len(a)
-i = 0
-while i < length:
-    sum, j = calc_sum(i, a, length)
-    if sum > max_sum:
-        max_sum = sum
-        max_i = i
-        max_j = j
-    i += 1
+def calc_sum(a):
+    max_i = 0
+    max_j = 0
+    max_sum = a[0]
+    length = len(a)
+    i = 0
+    while i < length:
+        sum, j = calc_sub_sum(i, a, length)
+        if sum > max_sum:
+            max_sum = sum
+            max_i = i
+            max_j = j
+        i += 1
+    return [max_sum, max_i, max_j]
 
-print(max_sum, max_i, max_j)
+a = [5]
+print(calc_sum(a))
+a = [5, 2]
+print(calc_sum(a))
+a = [5, 2, -1]
+print(calc_sum(a))
+a = [-1, 5, 2, -1]
+print(calc_sum(a))
+a = [-1, 5, 2, -1, -8, 2, 7]
+print(calc_sum(a))
+a = [-8, 4, -1, 5, -3, 8, -2, -10, 9]
+print(calc_sum(a))
